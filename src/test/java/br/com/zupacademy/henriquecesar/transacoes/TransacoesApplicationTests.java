986cfe4f259aa273c0e5b215da807e1073c6ba4f
Henrique César <henrique.jose@zup.com.br>
2021-11-08 16:25:49 -0300

    estrutura inicial do projeto

---
+++ src/test/java/br/com/zupacademy/henriquecesar/transacoes/TransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.henriquecesar.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
